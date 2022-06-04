5d97c9c33d681a7b3f9b983d30a94aea767fcd33
austineonyi <austineonyeka@gmail.com>
2022-06-04 01:56:13 +0100

    5-print_numbers.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,12 @@ int main(void)
 {
 	char ch = 'a';
 
-	while (ch <= 'z')
-	{
+	while (ch <= 'z')									{
 		if (ch != 'e' && ch != 'q')
-		{
-														putchar(ch);
-													}
-		ch++;										}
+		{												putchar(ch);
+		}
+		ch++;
+	}
 	putchar('\n');
 	return (0);
 }
